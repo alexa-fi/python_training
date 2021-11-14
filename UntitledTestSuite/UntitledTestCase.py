2a6cbd077d20046c7abf5762ce5e74087fa7be9a
alexa-fi <5957099@gmail.com>
2021-11-14 19:26:56 +0300

    upd req

--- UntitledTestSuite/UntitledTestCase.py
+++ UntitledTestSuite/UntitledTestCase.py
@@ -7,58 +7,48 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return true
+    except:
+        return false
+
+    
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r'')
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+        self.wd = webdriver.Chrome(executable_path=r'C:\Users\User\AppData\Local\Google\Chrome\Application\chrome.exe')
+        self.wd.implicitly_wait(30)
     
     def test_untitled_test_case(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/group.php")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("test2")
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("test2")
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("test2")
-        driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("group page").click()
-        driver.get("https://mail.google.com/mail/u/0/#inbox/FMfcgzGlksCSgQkRhZmqZkmsrnhHBVSG")
-        driver.find_element_by_link_text("Logout").click()
+        wd = self.wd
+        wd.get("http://localhost/addressbook/group.php")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("hfhf")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("fhfh")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("fjjf")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
+        try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
+
+
     
     def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
+        self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
